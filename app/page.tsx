facddb68e6227f92b62676b70c2cb9ae525fe85e
Manu M Benakal <manubenakal@gmail.com>
2024-09-20 21:21:14 +0530

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,97 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+    <div className="w-full h-screen bg-no-repeat bg-cover bg-center bg-[url('https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/himalayan/gallery/webp/thumbnail/gallery-2.webp')]">
+      <header className='lg:px-16 px-4 flex flex-wrap items-center py-4 shadow-sm'>
+        <div className='flex-1 flex justify-between items-center mix-blend-screen'>
+          <a href='#' className='text-4xl text-gray-400 font-extrabold'>
+            MOTOMAVERICKS
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <label htmlFor='menu-toggle' className='pointer-cursor md:hidden block'>
+          <svg
+            className='fill-current text-gray-600'
+            xmlns='http://www.w3.org/2000/svg'
+            width='20'
+            height='20'
+            viewBox='0 0 20 20'
+          >
+            <title>menu</title>
+            <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'></path>
+          </svg>
+        </label>
+        <input className='hidden' type='checkbox' id='menu-toggle' />
+
+        <div
+          className='hidden md:flex md:items-center md:w-auto w-full'
+          id='menu'
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <nav>
+            <ul className='md:flex items-center justify-between text-base text-white pt-4 md:pt-0'>
+              <li>
+                <a className='md:p-4 py-3 px-0 block' href='#'>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className='md:p-4 py-3 px-0 block' href='#'>
+                  Services
+                </a>
+              </li>
+              <li>
+                <a className='md:p-4 py-3 px-0 block' href='#'>
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a className='md:p-4 py-3 px-0 block md:mb-0 mb-2' href='#'>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <div className='w-[90%] mx-auto h-full flex items-center justify-between py-10'>
+        <div className='lg:w-fit'>
+          <div className='sm:text-6xl xs:text-5xl text-left text-white font-serif font-extrabold uppercase'>
+            <h1></h1>
+            <h1>MOTO</h1>
+            <h1 className='bg-black/30 text-white rounded-sm px-1 shadow-sm shadow-white/50'>
+              MAVERICKS
+            </h1>
+          </div>
+
+          <p className='text-md text-white bg-black/30 font-semibold mt-1 capitalize rounded-lg p-2'>
+            SINCE 2023
+          </p>
+        </div>
+
+        <div>
+          <ul className='text-3xl text-white'>
+            <li className='flex justify-center items-center p-1 bg-black/40 rounded-full'>
+              {/* <ion-icon name="logo-facebook"></ion-icon> */}
+            </li>
+            <li className='flex justify-center items-center p-1 bg-black/40 rounded-full mt-2'>
+              {/* <ion-icon name="logo-instagram"></ion-icon> */}
+            </li>
+            <li className='flex justify-center items-center p-1 bg-black/40 rounded-full mt-2'>
+              {/* <ion-icon name="logo-whatsapp"></ion-icon>
+                  </ion-icon> */}
+            </li>
+            <li className='flex justify-center items-center p-1 bg-black/40 rounded-full mt-2'>
+              {/* <ion-icon name="person-circle-outline"></ion-icon> */}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
+}
+{
+  /* <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script> */
 }
